243211ac7e68cb8c3dcbd525bf202e0b92025208
zardokar <nattaworn@hotmail.com>
2023-12-13 18:15:30 +0700

    [:hammer:] remove slash

--- libs/shared/environment/src/environment.ts
+++ libs/shared/environment/src/environment.ts
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
-const DOMAIN_PROD_OCN      = 'https://ksp-ocnapi.ksp.or.th/';
+const DOMAIN_PROD_OCN      = 'https://ksp-ocnapi.ksp.or.th';
 const DOMAIN_PROD_SELF_API = 'https://ksp-api.ksp.or.th';
 const DOMAIN_PROD_SELF     = 'https://ksp-self.ksp.or.th';
 
