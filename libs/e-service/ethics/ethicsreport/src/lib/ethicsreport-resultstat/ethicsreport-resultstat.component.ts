5934f84531e128d8bec8688c2d9263f52bb6881a
zardokar <nattaworn@hotmail.com>
2024-01-10 02:49:08 +0700

    [:hammer:] create EthicsReportResultStatistic Mock up

--- libs/e-service/ethics/ethicsreport/src/lib/ethicsreport-resultstat/ethicsreport-resultstat.component.ts
+++ libs/e-service/ethics/ethicsreport/src/lib/ethicsreport-resultstat/ethicsreport-resultstat.component.ts
@@ -4,12 +4,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Subject } from 'rxjs';
 
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { TopNavComponent } from '@ksp/shared/menu';
 import { LoaderService } from '@ksp/shared/service';
-// --------------------------------------------------------------------------------------------------------------------------
 
+import { GNXTableComponent } from '@ksp/shared/utility';
+
+// --------------------------------------------------------------------------------------------------------------------------
+const REPORT_COLS = [
+                      "วันเดือนปีที่บันทึก",
+                      "จำนวน"
+]
+// --------------------------------------------------------------------------------------------------------------------------
 @Component({
     templateUrl: './ethicsreport-resultstat.component.html',
     styleUrls: ['./ethicsreport-resultstat.component.scss'],
@@ -17,13 +25,44 @@ import { LoaderService } from '@ksp/shared/service';
     imports: [
       CommonModule,
       MatProgressSpinnerModule,
-      TopNavComponent
+      MatDatepickerModule,
+      ReactiveFormsModule,
+      TopNavComponent,
+      GNXTableComponent
     ],
 })
 
 export class EthicsReportResultStatisticComponent {
-  HEAD_LABEL : string = 'รายงานสถิติผลการพิจารณาและผลคำวินิจฉัย'
+
+  HEAD_LABEL : string         = 'รายงานสถิติผลการพิจารณาและผลคำวินิจฉัย'
+  REPORT_COLS_DATA : string[] = REPORT_COLS
+  TABLE_STYLE : any           = {
+                                  'width' : 'max-content'
+                                }
+
+  reportdata : any[any]       = [
+    { title: 'กรณี 13/1 ไม่ร้ายแรง วินิจฉัยโทษตักเตือน', count: '10'},
+    { title: 'กรณี 13/1 ไม่ร้ายแรง วินิจฉัยโทษภาคทัณฑ์', count: '10'},
+    { title: 'กรณี 60/5 ไม่ร้ายแรง วินิจฉัยโทษตักเตือน', count: '10'},
+    { title: 'กรณี 60/5 ไม่ร้ายแรง วินิจฉัยโทษภาคทัณฑ์', count: '10'},
+    { title: 'กรณี 60/6 ร้ายแรง วินิจฉัยโทษพักใช้', count: '10'},
+    { title: 'กรณี 60/6 ร้ายแรง วินิจฉัยโทษเพิกถอน', count: '10'},
+    { title: 'ไม่รับเรื่องพิจารณาและจำหน่ายออก เนื่องจากอายุความเกิน 1 ปี', count: '10'},
+    { title: 'ยกข้อกล่าวหา/กล่าวโทษ', count: '10'},
+    { title: 'ตักเตือน', count: '10'},
+    { title: 'ภาคทัณฑ์', count: '10'},
+    { title: 'พักใช้', count: '10'},
+    { title: 'เพิกถอนหนังสืออนุญาตประกอบวิชาชีพ', count: '10'},
+    { title: 'มติคณะกรรมการอื่นๆ', count: '10'},
+  ]
+
   isLoading: Subject<boolean> = this.loaderService.isLoading;
+
+  form = this.fb.group({
+    accusation_type: [],
+    fromDate: [],
+    toDate: []
+  });
 
   constructor(
               public router: Router,
@@ -32,4 +71,11 @@ export class EthicsReportResultStatisticComponent {
               private loaderService: LoaderService
   )
   {}
+
+  onClickSearch() {
+
+  }
+  clear() {
+  
+  }
 }
