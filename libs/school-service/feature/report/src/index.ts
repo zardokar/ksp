aef46f845c40faccf7bb47eb9e2dfd9646ac48b3
Koh <nattaworn@hotmail.com>
2024-03-21 21:15:46 +0700

    [:tada:] add temp license report page and menu

--- libs/school-service/feature/report/src/index.ts
+++ libs/school-service/feature/report/src/index.ts
@@ -3,3 +3,9 @@ export * from './lib/school-service-feature-report.module';
 export * from './lib/temp-license-report/temp-license-report.component';
 
 export * from './lib/test-report/test-report.component'
+
+export * from './lib/temp-license-person-thai-report/temp-license-person-thai-report.component'
+
+export * from './lib/temp-license-list-thai-report/temp-license-list-thai-report.component'
+
+export * from './lib/temp-license-list-forg-report/temp-license-list-forg-reportcomponent'
--- libs/school-service/feature/report/src/lib/school-service-feature-report.module.ts
+++ libs/school-service/feature/report/src/lib/school-service-feature-report.module.ts
@@ -9,6 +9,10 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { TestReportComponent } from './test-report/test-report.component';
+import { TempLicensePersonForgReportComponent } from './temp-license-person-forg-report/temp-license-person-forg-report.component';
+import { TempLicensePersonThaiReportComponent } from './temp-license-person-thai-report/temp-license-person-thai-report.component';
+import { TempLicenseListThaiReportComponent } from './temp-license-list-thai-report/temp-license-list-thai-report.component';
+import { TempLicenseListForgReportComponent } from './temp-license-list-forg-report/temp-license-list-forg-reportcomponent';
 import { TempLicenseReportComponent } from './temp-license-report/temp-license-report.component';
 
 const routes: Routes = [
@@ -26,6 +30,10 @@ const routes: Routes = [
         component: TempLicenseReportComponent,
       },
       { path: 'test-report', component: TestReportComponent },
+      { path: 'temp-license-person-thai', component: TempLicensePersonForgReportComponent },
+      { path: 'temp-license-person-forg', component: TempLicensePersonThaiReportComponent },
+      { path: 'temp-license-list-thai', component: TempLicenseListThaiReportComponent },
+      { path: 'temp-license-list-forg', component: TempLicenseListForgReportComponent },
     ],
   },
 ];
@@ -42,11 +50,19 @@ const routes: Routes = [
   ],
   declarations: [
     TestReportComponent,
-    TempLicenseReportComponent
+    TempLicenseReportComponent,
+    TempLicensePersonThaiReportComponent,
+    TempLicensePersonForgReportComponent,
+    TempLicenseListThaiReportComponent,
+    TempLicenseListForgReportComponent
   ],
   exports: [
     TestReportComponent,
-    TempLicenseReportComponent
+    TempLicenseReportComponent,
+    TempLicensePersonThaiReportComponent,
+    TempLicensePersonForgReportComponent,
+    TempLicenseListThaiReportComponent,
+    TempLicenseListForgReportComponent
   ],
 })
 export class SchoolServiceFeatureReportModule {}
